7ac212d48f19cda6802c7911fb745a890e5b534f
carlos <carlosgalves003@gmail.com>
2026-03-05 15:14:28 +0000

    worker fix

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 import os
 import uvicorn
 from app.core.config import settings
-from app.main import app
 
 if __name__ == "__main__":
     workers = int(os.getenv("WEB_CONCURRENCY", "1"))
     uvicorn.run(
-        app,
+        "app.main:app",
         host=settings.API_HOST,
         port=settings.port,
         workers=workers,
